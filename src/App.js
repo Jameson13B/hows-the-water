0611c71098aefc2fb5111580ed1d58da185d086e
Jameson Brown <jamesonbrown@neolivity.com>
2019-08-18 16:41:28 -0600

    Refactored NavBar to separate component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Link, Route, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import CallToAction from './components/CallToAction'
 import Home from './components/Home'
 import ErrorPage from './components/ErrorPage'
 import See from './components/See'
 import Share from './components/Share'
+import NavBar from './components/NavBar'
 
 class App extends React.Component {
   constructor(props) {
@@ -52,20 +53,7 @@ class App extends React.Component {
           <Route exact path='/share' component={Share} />
           <Route component={ErrorPage} />
         </Switch>
-        <NavBar id='navBar'>
-          <NavItem to='/'>
-            <i class='material-icons'>home</i>Home
-          </NavItem>
-          <NavItem to='/see'>
-            <i class='material-icons'>all_inbox</i>See
-          </NavItem>
-          <NavItem to='/share'>
-            <i class='material-icons'>create</i>Share
-          </NavItem>
-          <NavItem to='/'>
-            <i class='material-icons'>help_outline</i>About
-          </NavItem>
-        </NavBar>
+        <NavBar />
       </AppContainer>
     )
   }
@@ -80,35 +68,6 @@ const AppContainer = styled.div`
   text-align: center;
   font-size: 1.25em;
 `
-const NavBar = styled.div`
-  background: #282c34;
-  bottom: -9999px;
-  display: flex;
-  justify-content: space-evenly;
-  position: fixed;
-  transition: bottom 1s ease;
-  visibility: hidden;
-  width: 100vw;
-  z-index: 1;
-`
-const NavItem = styled(Link)`
-  border: 3px solid white;
-  border-radius: 10px;
-  color: white;
-  display: flex;
-  flex-direction: column;
-  font-size: 0.75em;
-  margin: 5px;
-  padding: 5px 0;
-  text-decoration: none;
-  width: 10%;
-  @media (max-width: 768px) {
-    width: 20%;
-  }
-  i {
-    font-size: 30px;
-  }
-`
 
 // To do list:
-// *Create about book and about Jameson view
+// Create about book and about Jameson view
---
+++ src/components/NavBar.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+
+const NavBar = () => {
+  return (
+    <Container id='navBar'>
+      <NavItem to='/'>
+        <i class='material-icons'>home</i>Home
+      </NavItem>
+      <NavItem to='/see'>
+        <i class='material-icons'>all_inbox</i>See
+      </NavItem>
+      <NavItem to='/share'>
+        <i class='material-icons'>create</i>Share
+      </NavItem>
+      <NavItem to='/'>
+        <i class='material-icons'>help_outline</i>About
+      </NavItem>
+    </Container>
+  )
+}
+
+export default NavBar
+
+const Container = styled.div`
+  background: #282c34;
+  bottom: -9999px;
+  display: flex;
+  justify-content: space-evenly;
+  position: fixed;
+  transition: bottom 0.5s ease;
+  visibility: hidden;
+  width: 100vw;
+  z-index: 1;
+`
+const NavItem = styled(Link)`
+  border: 3px solid white;
+  border-radius: 10px;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  font-size: 0.75em;
+  margin: 5px;
+  padding: 5px 0;
+  text-decoration: none;
+  width: 10%;
+  @media (max-width: 768px) {
+    width: 20%;
+  }
+  i {
+    font-size: 30px;
+  }
+`
